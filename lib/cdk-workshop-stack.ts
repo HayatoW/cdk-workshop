a834635a63364a24643eab70cbe823b30814052a
Hayato Watanabe <solideye@me.com>
2021-07-18 01:28:33 +0900

    add a table property to our hit counter

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
 import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -25,5 +26,9 @@ export class CdkWorkshopStack extends cdk.Stack {
       handler: helloWithCounter.handler
     });
     
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table
+    })
   }
 }
